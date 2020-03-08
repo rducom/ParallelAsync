f03d994cfaf627472e840955b4f427722b030e4b
Raph <raphael.ducom@gmail.com>
2020-03-08 15:15:26 +0100

    initial code

---
+++ src/AsyncStreamExtensions.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ParallelAsync
+{
+    public static partial class AsyncStreamExtensions
+    {
+        public static AsyncStream<T> AsAsyncStream<T>(this IEnumerable<T> source, CancellationToken cancellationToken)
+        {
+            var channel = Channel.CreateUnbounded<StreamedValue<T>>(new UnboundedChannelOptions
+            {
+                SingleWriter = true,
+                SingleReader = false
+            });
+            return new AsyncStream<T>(channel, cancellationToken, async () =>
+            {
+                await AsAsyncStreamInternalAsync(source, cancellationToken, channel);
+            });
+        }
+
+        public static AsyncStream<T> AsAsyncStream<T>(this IAsyncEnumerable<T> source, CancellationToken cancellationToken)
+        {
+            var channel = Channel.CreateUnbounded<StreamedValue<T>>(new UnboundedChannelOptions
+            {
+                SingleWriter = true,
+                SingleReader = false
+            });
+            return new AsyncStream<T>(channel, cancellationToken, async () =>
+            {
+                await AsAsyncStreamInternalAsync(source, cancellationToken, channel);
+            });
+        }
+
+        public static AsyncStream<T> CountAsync<T>(this AsyncStream<T> source, out Counter counter)
+        {
+            var localCounter = new Counter();
+            counter = localCounter;
+            var channel = Channel.CreateUnbounded<StreamedValue<T>>(new UnboundedChannelOptions
+            {
+                SingleWriter = false,
+                SingleReader = false
+            });
+            return new AsyncStream<T>(channel, source.CancellationToken, async () =>
+            {
+                await CountInternalAsync(source, localCounter, channel); 
+            });
+        }
+
+        public static AsyncStream<T> ForEachAsync<T>(this AsyncStream<T> source, Func<StreamedValue<T>, CancellationToken, Task> action)
+        {
+            var channel = Channel.CreateUnbounded<StreamedValue<T>>(new UnboundedChannelOptions
+            {
+                SingleWriter = false,
+                SingleReader = false
+            });
+            return new AsyncStream<T>(channel,  source.CancellationToken, async () =>
+            {
+                await ForeachInternalAsync(source, action, channel); 
+            });
+        }
+
+
+        public static async Task<IReadOnlyCollection<T>> AsReadOnlyCollectionAsync<T>(this AsyncStream<T> source)
+        {
+            var items = new ConcurrentBag<T>();
+            await foreach (var item in source.ChannelReader.ReadAllAsync(source.CancellationToken))
+            {
+                items.Add(item.Item);
+            }
+            return items;
+        }
+
+        private static async Task AsAsyncStreamInternalAsync<T>(IEnumerable<T> source, CancellationToken cancellationToken, Channel<StreamedValue<T>> channel)
+        {
+            try
+            {
+                foreach (var item in source)
+                {
+                    await channel.Writer.WriteAsync(new StreamedValue<T>(item, ExecutionStatus.Succeeded), cancellationToken);
+                }
+            }
+            finally
+            {
+                channel.Writer.Complete();
+            }
+        }
+
+        private static async Task AsAsyncStreamInternalAsync<T>(IAsyncEnumerable<T> source, CancellationToken cancellationToken, Channel<StreamedValue<T>> channel)
+        {
+            try
+            {
+                await foreach (var item in source)
+                {
+                    await channel.Writer.WriteAsync(new StreamedValue<T>(item, ExecutionStatus.Succeeded), cancellationToken);
+                }
+            }
+            finally
+            {
+                channel.Writer.Complete();
+            }
+        }
+
+        private static async Task CountInternalAsync<T>(AsyncStream<T> source, Counter localCounter, Channel<StreamedValue<T>> channel)
+        {
+            try
+            {
+                await foreach (var item in source.ChannelReader.ReadAllAsync(source.CancellationToken))
+                {
+                    localCounter.Increment();
+                    await channel.Writer.WriteAsync(item, source.CancellationToken);
+                }
+            }
+            finally
+            {
+                channel.Writer.Complete();
+            }
+        }
+
+        private static async Task ForeachInternalAsync<T>(AsyncStream<T> source, Func<StreamedValue<T>, CancellationToken, Task> action, Channel<StreamedValue<T>> channel)
+        {
+            try
+            {
+                await foreach (var item in source.ChannelReader.ReadAllAsync(source.CancellationToken))
+                {
+                    await action(item, source.CancellationToken);
+                    await channel.Writer.WriteAsync(item, source.CancellationToken);
+                }
+            }
+            finally
+            {
+                channel.Writer.Complete();
+            }
+        }
+    }
+
+    public class Counter
+    {
+        private int _count;
+        public int Count => _count;
+
+        public void Increment()
+        {
+            Interlocked.Increment(ref _count);
+        }
+    }
+}
---
+++ tests/ListParallelTest.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ParallelAsync.Tests
+{
+    public class ParallelAsyncBaseTest
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(16)]
+        [InlineData(32)]
+        [InlineData(128)]
+        public async Task ParallelTestAsync(int degree)
+        {
+            var tasks = Enumerable.Range(0, 2000).Select(i => 2).ToList();
+            tasks.Add(200);
+            tasks.Add(100);
+            tasks.Add(300);
+            var cts = new CancellationTokenSource();
+            int count = 0;
+            int max = 0;
+            var option = new ParallelizeOption
+            {
+                FailMode = Fail.Smart,
+                MaxDegreeOfParallelism = degree
+            };
+            await tasks.ParallelizeAsync(async (i, ct) =>
+            {
+                int loopMax = Interlocked.Increment(ref count);
+                max = Math.Max(max, loopMax);
+                await Task.Delay(i, ct);
+                Interlocked.Decrement(ref count);
+                Assert.True(count <= degree);
+                return true;
+            }, option, cts.Token).Task;
+
+            Assert.Equal(degree, max);
+        }
+
+
+        [Theory]
+        [InlineData(Fail.Default)]
+        [InlineData(Fail.Fast)]
+        [InlineData(Fail.Smart)]
+        public async Task CancellationAsync(Fail failMode)
+        {
+            var tasks = Enumerable.Range(0, 10).ToList();
+            var cts = new CancellationTokenSource();
+            int index = 0;
+            var option = new ParallelizeOption
+            {
+                FailMode = failMode,
+                MaxDegreeOfParallelism = 8
+            };
+            var pTask = tasks.ParallelizeAsync(async (i, ct) =>
+            {
+                int ix = Interlocked.Increment(ref index);
+                if (ix == 4)
+                {
+                    cts.Cancel();
+                }
+                else
+                {
+                    try
+                    {
+                        await Task.Delay(1000, ct);
+                    }
+                    catch (Exception)
+                    {
+                        // Ignore
+                    }
+
+                    if (ct.IsCancellationRequested)
+                    {
+                        return false;
+                    }
+                    Assert.False(true);
+                }
+                return true;
+            }, option, cts.Token);
+
+            if (failMode == Fail.Default)
+            {
+                await Assert.ThrowsAsync<TaskCanceledException>(async () =>
+                {
+                    await pTask.Task;
+                });
+            }
+            else
+            {
+                var result = await pTask.GetExceptionsAsync();
+                Assert.True(result.IsCanceled);
+            }
+        }
+
+        [Theory]
+        [InlineData(Fail.Default)]
+        [InlineData(Fail.Fast)]
+        [InlineData(Fail.Smart)]
+        public async Task ExceptionAsync(Fail failMode)
+        {
+            var tasks = Enumerable.Range(0, 10).ToList();
+            int index = 0;
+            bool badBehavior = false;
+            var option = new ParallelizeOption
+            {
+                FailMode = failMode,
+                MaxDegreeOfParallelism = 8
+            };
+            var pTask = tasks.ParallelizeAsync(async (i, ct) =>
+            {
+                int ix = Interlocked.Increment(ref index);
+                if (ix == 4)
+                {
+                    throw new TestException();
+                }
+
+                await Task.Delay(1000, ct);
+                if (ct.IsCancellationRequested)
+                {
+                    return false;
+                }
+                if (failMode == Fail.Fast)
+                {
+                    badBehavior = true;
+                }
+                return true;
+            }, option, CancellationToken.None);
+
+            if (failMode == Fail.Default)
+            {
+                await Assert.ThrowsAsync<TestException>(async () =>
+                {
+                    await pTask.Task;
+                });
+                Assert.Equal(10, index);
+            }
+            else
+            {
+                ParallelizedSummary result = await pTask.GetExceptionsAsync();
+                Assert.False(result.IsSuccess);
+                Assert.False(result.IsCanceled);
+                Assert.NotEmpty(result.Exceptions);
+            }
+
+            if (badBehavior)
+            {
+                Assert.True(false);
+            }
+        }
+
+        public sealed class TestException : Exception { }
+    }
+}
